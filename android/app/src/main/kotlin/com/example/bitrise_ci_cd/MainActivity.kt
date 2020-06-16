a84bbb36379cff818214cfa1ec158ceac19f116c
vyppN <chwrtokuma@gmail.com>
2020-06-16 13:51:18 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/bitrise_ci_cd/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.vyppn.bitrise_ci_cd
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
